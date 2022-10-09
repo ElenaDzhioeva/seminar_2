21972f5785ba83c043ff8cfa173f5836b390b61e
ElenaDzhioeva <115354785+ElenaDzhioeva@users.noreply.github.com>
2022-10-09 09:09:40 +0300

    Update homework.R

--- homework.R
+++ homework.R
@@ -1,60 +1,66 @@
-install.packages("xlsx", dep = T)
+install.packages("readxl")
 install.packages("rpivotTable")
 install.packages("dplyr")
 
-library("xlsx")
-library("rpivotTable")
-library("dplyr")
+library(readxl)
+library(rpivotTable)
+library(dplyr)
 
-data <- read.xlsx("problems_1.xlsx", sheetIndex = "0", )
-df <- data.frame(data)
-View(df)
+pb <- readxl::read_xlsx("problems_1.xlsx")
 
-df$TotalAmount <- as.numeric(df$TotalAmount)
-df$Year <- as.numeric(df$Year)
+View(pb)
 
-# 1 - Средняя сумма начисленных средств в разрезе административных округов
-first <- df %>% group_by(AdmArea, Year) %>% 
+
+#расчитать среднюю сумму начисленных средств(TotalAmount) в разрезе административных округов с детализацией по 2016, 2017, 2018, 2019 гг.
+
+pb2016 <- pb[pb$Year==2016:2019,]
+
+View(pb2016)
+
+rpivotTable::rpivotTable(pb2016, rows = c("AdmArea", "Year"), aggregatorName = "Average", vals = "TotalAmount")
+
+
+#рассчитать суммарные помесячные начисления за 2021 год.
+
+pb2021 <- pb[pb$Year==2021,]
+
+View(pb2021)
+
+rpivotTable::rpivotTable(pb2021, rows="Month", cols="Year", aggregatorName = "Sum",vals="TotalAmount")
+
+
+#построить график средних ежегодных начислений по всем административным округам.
+
+rpivotTable::rpivotTable(pb, rows="AdmArea", aggregatorName ="Average", vals="TotalAmount", rendererName = "Bar Chart")
+
+
+#из исходных данных отобрать только центральный административный округ, создав из него новый датафрейм.
+
+central <- pb[pb$AdmArea=="Центральный административный округ",]
+
+View(central)
+
+write.csv(central, file = "central.csv", fileEncoding = "UTF-8")
+
+#в созданном датафрейме отобрать только те месяца, начисления в которых оказались выше среднего уровня начислений в течение соответствующего года.
+
+central$TotalAmount <- as.numeric(central$TotalAmount)
+central$Year <- as.numeric(central$Year)
+
+central_average <- central %>% group_by(AdmArea, Year) %>% 
   summarise(average_sum = mean(TotalAmount))
-View(first)
-rpivotTable::rpivotTable(df,cols=c("Year"), rows=c("AdmArea"), aggregatorName = "Average",vals="TotalAmount", rendererName = "Bar Chart")
 
-# 2 - Суммарные помесячные начисления
-rpivotTable::rpivotTable(df, cols=c("Year"), rows=c("Month"), aggregatorName = "Sum",vals="TotalAmount", rendererName = "Bar Chart")
-# 2 - Суммарные помесячные начисления за 2021 г.
-rpivotTable::rpivotTable(df, cols=c("Year" == 2021), rows=c("Month"), aggregatorName = "Sum",vals="TotalAmount", rendererName = "Bar Chart")
+View(central_average)
 
-# 3
-#сумма ежегодных начислений по всем округам
-third <- df %>% group_by(AdmArea, Year) %>% 
-  summarise(sum_year = sum(TotalAmount))
-View(third) 
-#среднегодовые начисления по округам
-rpivotTable::rpivotTable(third, rows=c("AdmArea"), aggregatorName = "Average",vals="sum_year", rendererName = "Bar Chart") 
-
-# ЦАО
-cad <- df[df$AdmArea == "Центральный административный округ",]
-View(cad)
-write.csv(cad, file = "cad.csv")
-
-#Среднемесячные начисления ЦАО
-cad_average <- first[first$AdmArea == "Центральный административный округ",]
-View(cad_average)
-
-cad <- cad %>% mutate(average_sum = recode(Year, 
-                                           '2016'="средняя сумма",
-                                           '2017'="1083549356",
-                                           '2018' ="1240896844",
-                                           '2019' = "1219241088",
-                                           '2020' = "1300439017",
-                                           '2021' = "1393470895"
+central <- central %>% mutate(average_sum = recode(Year, 
+'2016'="934177571", '2017'="1083549356", '2018' ="1240896844",
+'2019' = "1219241088", '2020' = "1300439017", '2021' = "1393470895"
 ))
-View(cad)
 
-cad$average_sum <- as.numeric(cad$average_sum)
-cad$TotalAmount <- as.numeric(cad$TotalAmount)
+central$average_sum <- as.numeric(central$average_sum)
 
-cad_2 <- cad[cad$TotalAmount > cad$average_sum, ]
-View(cad_2)
-write.csv(cad_2, file = "cad_2.csv")
+central_months <- central[central$TotalAmount > central$average_sum,]
 
+View(central_months)
+
+write.csv(central_months, file = "central_months.csv", fileEncoding = "UTF-8")
